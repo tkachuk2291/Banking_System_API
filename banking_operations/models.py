1d2385b8720118e6ccb3b384d5fe9af3edc4d9e1
Anton Tkachuk <tkacuk2291@gmail.com>
2024-06-07 16:44:54 +0300

    created user_account app

--- banking_operations/models.py
+++ banking_operations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Deposit(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        pass
